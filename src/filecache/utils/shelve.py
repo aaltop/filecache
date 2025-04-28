a78c0dc2a2e8e7df00d9db2a33458115dbecd159
TA <aaltopi@gmail.com>
2025-04-28 14:36:53 +0300

    Add implementation for deleting dbm.gnu files

--- src/filecache/utils/shelve.py
+++ src/filecache/utils/shelve.py
@@ -2,9 +2,12 @@ import shelve
 from pathlib import Path
 import dbm
 from typing import TypedDict
+import logging
 
 from ..exceptions import DatabaseReadError
 
+
+logger = logging.getLogger(__name__)
 
 def save_dict(save_path: Path, _dict: dict) -> None:
     """
@@ -56,7 +59,7 @@ def clear_shelve(save_path: Path) -> DeletedDatabase:
     # talks of .dir and .pag, though dbm.dumb's one doesn't mention .bak,
     # which it does include, so hard to say. Would have to test)
     match db_type:
-        case "dbm.sqlite3":
+        case "dbm.sqlite3" | "dbm.gnu":
             try:
                 save_path.unlink(missing_ok=False)
                 deletion_info["deleted_files"][save_path] = True
@@ -83,10 +86,10 @@ def clear_shelve(save_path: Path) -> DeletedDatabase:
 
         case None:
             raise DatabaseReadError("Could not read database file")
-
         # should be the last two dbm possibilities, for which
         # there is obviously no implementation right now
         case _:
-            raise NotImplementedError
+            logger.exception("Unknown db file structure: %s", list(save_path.parent.iterdir()))
+            raise NotImplementedError("shelve clearing is not implemented for", db_type)
 
     return deletion_info
